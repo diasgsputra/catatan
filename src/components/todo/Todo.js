5d72650b555f9e8c3c6a54169599b2c083b387cd
diasgsputra <60877754+diasgsputra@users.noreply.github.com>
2021-10-13 11:45:15 +0700

    first commit

---
+++ src/components/todo/Todo.js
@@ -0,0 +1,25 @@
+import './Todo.css'
+import {useState} from "react"
+import TodoList from "../todo-list/TodoList"
+import TodoCreate from '../todo-create/TodoCreate'
+
+const Todo = () => {
+    const [getTodos, setTodos] = useState([
+        
+    ])
+
+    const eventCreateTodo = (todo) => {
+        setTodos(getTodos.concat(todo))
+        console.log(getTodos)
+    }
+
+    return(
+        <div>
+            <h3>Catatan</h3>
+            <TodoCreate onCreateTodo={eventCreateTodo}/>
+            <TodoList dataTodos={getTodos}/>
+        </div>
+    )
+}
+
+export default Todo
